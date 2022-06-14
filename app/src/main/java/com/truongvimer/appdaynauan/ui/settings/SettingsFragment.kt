6d0da222e674d1be20ef6e5321d923a7343eca58
TruongNguyenTomosia <truong.nguyen@tomosia.com>
2022-06-14 09:52:54 +0700

    First Change

---
+++ app/src/main/java/com/truongvimer/appdaynauan/ui/settings/SettingsFragment.kt
@@ -0,0 +1,32 @@
+package com.truongvimer.appdaynauan.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.truongvimer.appdaynauan.R
+
+class SettingsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
+
+    private lateinit var viewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/truongvimer/appdaynauan/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.truongvimer.appdaynauan.ui.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
